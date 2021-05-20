3a382daa628002099581872a8c3f41930341ef6d
nekolr <excalibll@163.com>
2021-05-20 21:40:17 +0800

    🎨 Add JsonProperty

--- slime-core/src/main/java/com/github/nekolr/slime/domain/dto/DataSourceDTO.java
+++ slime-core/src/main/java/com/github/nekolr/slime/domain/dto/DataSourceDTO.java
@@ -1,6 +1,6 @@
 package com.github.nekolr.slime.domain.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -44,7 +44,7 @@ public class DataSourceDTO {
     /**
      * 密码
      */
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     /**
